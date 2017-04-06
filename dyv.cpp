ce299727592ff9e077cf30b9aba17890f58c694d
tonigit <tonimanusr@gmail.com>
2017-04-06 20:13:57 +0200

    dyv comentado

--- dyv.cpp
+++ dyv.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 
+//Algoritmo general, dividimos en 2 partes
 solucion divideVenceras(int i, int j) {
 	int p;
 	if (pequeno(i,j)) {
@@ -14,33 +15,43 @@ solucion divideVenceras(int i, int j) {
 	}
 }
 
+//Es pequeño cuando el tamaño del array es de 1 posicion
 bool pequeno (int i, int j){
 	if (i == j) {
 		return true;
 	} else return false;
 }
 
+/* Calculamos la suma de la diferencia y lo almacenamos en nuestra estructura de datos
+donde pos es la inicial (0), el valor es -1 si es caso base. además tenemos un vector
+auxiliar en el que tenemos el calculo para para cada posicion */
 solucion solucionDirecta(int i) {
-	//Rellenar array C con los datos contenidos en A y B
 	solucion s1;
 	s1.pos = i;
+	// -1 si es caso base
 	s1.valor = -1;
+	//Suma de la diferencia en valor absoluto
 	s1.diferencias.insert(s1.diferencias.begin(),abs(int(a[i]) - int(b[i])));
 
 	return s1;
 }
 
+/* Dividimos en 2 partes, si la división no es entera, la subcadena de mayor longitud
+irá a la izquierda */
 int dividir(int i, int j) {
 	int aux = j - i + 1;
+	// División y módulo
 	int p = ((i + (aux/2)) + (aux%2) - 1);
 	return p;
 }
 
+// Combina las soluciones dependiendo de m
 solucion combinar(solucion s1, solucion s2) {
 
 	solucion s3;
 	s3.diferencias.clear();
-
+	
+	// Juntamos las dos estructuras en una sola
 	for (int i = 0; i < s1.diferencias.size(); ++i)
 	{
 		s3.diferencias.push_back(s1.diferencias.at(i));
@@ -51,11 +62,13 @@ solucion combinar(solucion s1, solucion s2) {
 		s3.diferencias.push_back(s2.diferencias.at(i));
 	}
 	int limite = s3.diferencias.size();
-
+	
+	// Si m es menor que el tamaño seguimos subiendo
 	if (limite < m) {
 		s3.pos = -1;
 		s3.valor = -1;
 		return s3;
+	// si m es igual al tamaño, se realiza la suma de toda la cadena
 	} else if (limite == m){
 		int sumaTotal=0;
 		for (int x = 0; x < m; ++x)
@@ -65,25 +78,24 @@ solucion combinar(solucion s1, solucion s2) {
 		s3.pos = 0;
 		s3.valor = sumaTotal;
 		return s3;
+	// m es mayor que el tamaño de la cadena
 	} else {
 		int indCompr = s1.diferencias.size() - m + 1;
 		int cont = 1;
 		int valorCompr = 0;
 		int sumaTotal = 0;
 		int posCompr = -1;
+		/* Se realizan las comprobaciones intermedias de la frontera desde la solucion
+		de s1 hasta la solucion de 2, al acabar nos quedamos con el mayor */
 		while ((indCompr + m) <= limite && cont < m) {
-			//cout << "while" << endl;
 			for (int x = 0; x < m; ++x)
 			{
 				int aux = x+indCompr;
-				//cout << "s1.dif " << s1.diferencias.size() << " s2.dif " << s2.diferencias.size() << endl;
-				//cout << "x " << x << " indCompr " << indCompr << " aux " << aux << endl;
 				if (indCompr > -1) {
 					sumaTotal = sumaTotal + s3.diferencias.at(x+indCompr);
 				}
 			}
 			if (sumaTotal > valorCompr) {
-				//cout << "actualiza " << indCompr << " " << sumaTotal << endl;
 				posCompr = indCompr;
 				valorCompr = sumaTotal;
 			}
@@ -91,29 +103,21 @@ solucion combinar(solucion s1, solucion s2) {
 			indCompr++;
 			cont++;
 		}
+		// Ahora nos quedamos con el mayor entre s1, s2 y el mayor obtenido arriba
 		int maximo = max(s1.valor,max(s2.valor,valorCompr));
-		//s3.valor = max(s1.valor,max(s2.valor,valorCompr));
 
-			//cout << "AQUI 0" << endl;
+		//Asignamos el valor y posicion correctos a la estructura solucion s3
 		if (maximo == s1.valor) {
-			//cout << "AQUI 1" << endl;
 			s3.valor = maximo;
 			if (s1.pos == -1) {
 				s3.pos = posCompr;
-
-			//cout << "AQUI 2" << endl;
 			} else {				
 				s3.pos = s1.pos;
-
-			//cout << "AQUI 3" << endl;
 			}
 		} else if (maximo == valorCompr) {
-			//cout << "AQUI 4 " << posCompr << " " << maximo << endl;
 			s3.pos = posCompr;
 			s3.valor = maximo;
 		} else {
-
-			//cout << "AQUI 5" << endl;
 			s3.valor = maximo;			
 			if (s2.pos == -1) {
 				s3.pos = posCompr;
